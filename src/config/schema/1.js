4338065eeaa96d96f7383b94c73db07e1f061ace
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-07-01 21:42:19 +0300

    Finished 1.0 config schema. Writing tests.

--- src/config/schema/1.js
+++ src/config/schema/1.js
@@ -1,6 +1,26 @@
 import backoff from './schema-parts/backoff';
 
 export default {
-  backoff,
-  
-}
+  type: 'object',
+  properties: {
+    accessTokenURL: {
+      type: 'url',
+    },
+    authURL: {
+      type: 'url',
+    },
+    requestTokenURL: {
+      type: 'url',
+    },
+    signatureMethod: {
+      type: 'string',
+      default: 'HMAC-SHA1',
+    },
+    version: {
+      type: 'string',
+      default: '1.0',
+    },
+    backoff,
+  },
+  required: ['accessTokenURL', 'authURL', 'requestTokenURL'],
+};
