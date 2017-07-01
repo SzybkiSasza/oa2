2cefcbb6e7207a606d6e5dc4c02727af0f86b272
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-07-01 21:09:26 +0300

    Chose AJV as the best JSON validator. Finished backoff schema part definition.

---
+++ src/config/schema/schema-parts/backoff.js
@@ -0,0 +1,29 @@
+export default {
+  id: '\Backoff',
+  type: 'object',
+  properties: {
+    retries: {
+      default: 10,
+      type: 'number',
+    },
+    factor: {
+      default: 2,
+      type: 'number',
+    },
+    minTimeout: {
+      default: 1000,
+      type: 'number',
+    },
+    maxTimeout: {
+      default: 10000,
+      type: 'number',
+      minimum: {
+        $data: '1/minTimeout',
+      },
+    },
+    randomize: {
+      default: true,
+      type: 'boolean',
+    },
+  },
+};
