7a4f6ee7eb7c6ef976d61a60969586bbdbef26de
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-07-05 01:41:27 +0300

    Added method for building Authorization URL.

--- src/2/index.js
+++ src/2/index.js
@@ -1,3 +1,6 @@
+import {each, join, union} from 'lodash';
+import uri from 'urijs';
+
 import validate from '../common/schema/validate';
 
 import clientSchema from './schema/client';
@@ -19,12 +22,33 @@ export class O2 {
    * Gets URL used for authorizing
    * @param  {Object} client          Client config
    * @param  {Object} optionals       Optional params to be passed to the URL
-   * @param  {String} optionals.state Additional state to pass to Auth autority
-   * @param  {String} optionals.responseType Flag to overload default resp. type
+   * @param  {Object} overrides       Overrides for the default configuration
+   * @param  {String} overrides.responseType Flag to overload default resp. type
+   * @param  {String} overrides.scope Optional scopes to be merged with defaults
    * @return {String}                 Authorization URL
    */
-  getAuthorizeURL(client, optionals) {
+  getAuthorizeURL(client, optionals = {}, overrides = {}) {
     client = validate(client, clientSchema);
-    return '';
+
+    // Allow for overriding response type per request, if needed
+    const responseType = overrides.responseType || this.config.responseType;
+
+    const URL = uri(this.config.authURL)
+      .addQuery('client_id', client.client_id)
+      .addQuery('redirect_uri', client.redirect_uri)
+      .addQuery('response_type', responseType);
+
+    // Build scope, if needed
+    const scope = union(overrides.scope || [], this.config.scope || []);
+    if (scope.length) {
+      URL.addQuery('scope', join(scope, ' '));
+    }
+
+    // Add optional parameters as they are
+    each(optionals, (param, key) => {
+      URL.addQuery(key, param);
+    });
+
+    return URL.toString();
   }
 }
--- src/2/index.test.js
+++ src/2/index.test.js
@@ -1,7 +1,135 @@
+jest.mock('../common/schema/validate', () => jest.fn((obj) => obj));
+
+import validate from '../common/schema/validate';
+
 import {O2} from '.';
 
 describe('OAuth 2 tests', () => {
   it('Is a defined class', () => {
     expect(O2).toBeInstanceOf(Function);
+  });
+
+  it('Validates input config', () => {
+    validate.mockClear();
+
+    const config = {
+      some: 'config',
+    };
+    new O2(config);
+
+    expect(validate).toHaveBeenCalledWith(config, expect.any(Object));
+  });
+
+  describe('getAuthorizeURL', () => {
+    it('Validates input parameters', () => {
+      const config = {
+        authURL: 'https://some-url',
+      };
+      const oauth2 = new O2(config);
+
+      validate.mockClear();
+
+      const client = {
+        client_id: 'abc',
+      };
+      oauth2.getAuthorizeURL(client);
+
+      expect(validate).toHaveBeenCalledWith(client, expect.any(Object));
+    });
+
+    it('Sets the default response type', () => {
+      const config = {
+        authURL: 'https://some-url',
+        responseType: 'code',
+      };
+      const client = {
+        client_id: 'abc',
+        redirect_uri: 'https://redir-uri.net',
+      };
+
+      const oauth2 = new O2(config);
+      validate.mockClear();
+
+      const URL = oauth2.getAuthorizeURL(client);
+      expect(URL).toEqual('https://some-url/?client_id=abc&' +
+        'redirect_uri=https%3A%2F%2Fredir-uri.net&response_type=code');
+    });
+
+    it('Allows for overriding response type', () => {
+      const config = {
+        authURL: 'https://some-url',
+        responseType: 'code',
+      };
+      const client = {
+        client_id: 'abc',
+        redirect_uri: 'https://redir-uri.net',
+      };
+
+      const oauth2 = new O2(config);
+      validate.mockClear();
+
+      const URL = oauth2.getAuthorizeURL(client, {}, {
+        responseType: 'token',
+      });
+      expect(URL).toEqual('https://some-url/?client_id=abc&' +
+        'redirect_uri=https%3A%2F%2Fredir-uri.net&response_type=token');
+    });
+
+    it('Builds the scope object', () => {
+      const config = {
+        authURL: 'https://some-url',
+        responseType: 'code',
+        scope: [
+          'one',
+        ],
+      };
+      const client = {
+        client_id: 'abc',
+        redirect_uri: 'https://redir-uri.net',
+      };
+
+      const oauth2 = new O2(config);
+      validate.mockClear();
+
+      const URL = oauth2.getAuthorizeURL(client, {}, {
+        responseType: 'token',
+        scope: [
+          'two',
+        ],
+      });
+      expect(URL).toEqual('https://some-url/?client_id=abc&' +
+        'redirect_uri=https%3A%2F%2Fredir-uri.net&response_type=token&' +
+        'scope=two+one');
+    });
+
+    it('Adds optional parameters as they are', () => {
+      const config = {
+        authURL: 'https://some-url',
+        responseType: 'code',
+        scope: [
+          'one',
+        ],
+      };
+      const client = {
+        client_id: 'abc',
+        redirect_uri: 'https://redir-uri.net',
+      };
+
+      const oauth2 = new O2(config);
+      validate.mockClear();
+
+      const URL = oauth2.getAuthorizeURL(client, {
+        optional: 'parameter',
+      }, {
+        responseType: 'token',
+        scope: [
+          'two',
+        ],
+      });
+      expect(URL).toEqual('https://some-url/?client_id=abc&' +
+        'redirect_uri=https%3A%2F%2Fredir-uri.net&response_type=token&' +
+        'scope=two+one&' +
+        'optional=parameter');
+    });
   });
 });
